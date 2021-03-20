fcc90b3bd5dc70e6ec465eaf34735d2103568907
Matias <mativink@gmail.com>
2021-03-20 16:57:40 +0100

    new commit 20/3

--- Trieur_Fichier_Téléchargements/trieur_fichiers.py
+++ Trieur_Fichier_Téléchargements/trieur_fichiers.py
@@ -27,7 +27,7 @@ master = tkinter.Tk()
 master.geometry("400x400")
 master.minsize(400,400)
 master.maxsize(400,400)
-master.config(background='#585555')
+master.config(background='#1E2334')
 master.title("Trieur du dossier Téléchargements")
 
 
@@ -111,6 +111,7 @@ def trieur_musique():
             path_final_msq+= elements
     commande_mp3 = 'move *.mp3 "' + path_final_msq + '"'
     commande_sfk = 'move *.sfk "' + path_final_msq + '"'
+    commande_wav = 'move *.wav "' + path_final_msq + '"'
     #on vérifie si un fichier existe déjà, sinon on le créer
     if os.path.exists(path_final_msq):
         print("dossier déjà existant")
@@ -119,6 +120,7 @@ def trieur_musique():
         print("Dossier non existant, création du dossier en cours")
     os.system(commande_mp3)
     os.system(commande_sfk)
+    os.system(commande_wav)
 
 #fonction qui trie les fichiers packages dans un dossier
 def trieur_packages():
@@ -140,6 +142,67 @@ def trieur_packages():
     os.system(commande_msi)
     os.system(commande_jar)
 
+#fonction qui trie les fichiers videos dans un dossier
+def trieur_videos():
+    """fonction qui trie les fichiers musique dans un dossier"""
+    downloads_end_path_msq = "\Downloads\TRI_Fichiers_Video"
+    x = path_default +nom_user+ downloads_end_path_msq
+    path_final_msq = ""
+    for elements in x:
+        if elements != " ":
+            path_final_msq+= elements
+    commande_mp4 = 'move *.mp4 "' + path_final_msq + '"'
+    commande_avi = 'move *.avi "' + path_final_msq + '"'
+    commande_mov = 'move *.mov "' + path_final_msq + '"'
+    #on vérifie si un fichier existe déjà, sinon on le créer
+    if os.path.exists(path_final_msq):
+        print("dossier déjà existant")
+    else:
+        os.mkdir(path_final_msq)
+        print("Dossier non existant, création du dossier en cours")
+    os.system(commande_mp4)
+    os.system(commande_avi)
+    os.system(commande_mov)
+
+#fonction qui trie les fichiers de polices d'ecriture dans un dossier
+def trieur_font():
+    """fonction qui trie les fichiers font dans un dossier"""
+    downloads_end_path_msq = "\Downloads\TRI_Fichiers_Polices_Ecriture"
+    x = path_default +nom_user+ downloads_end_path_msq
+    path_final_msq = ""
+    for elements in x:
+        if elements != " ":
+            path_final_msq+= elements
+    commande_otf = 'move *.otf "' + path_final_msq + '"'
+    #on vérifie si un fichier existe déjà, sinon on le créer
+    if os.path.exists(path_final_msq):
+        print("dossier déjà existant")
+    else:
+        os.mkdir(path_final_msq)
+        print("Dossier non existant, création du dossier en cours")
+    os.system(commande_otf)
+
+#fonction qui trie les fichiers de code (py,c) dans un dossier
+def trieur_codeFile():
+    """fonction qui trie les fichiers py,c, dans un dossier"""
+    downloads_end_path_msq = "\Downloads\TRI_Fichiers_CodeFiles"
+    x = path_default +nom_user+ downloads_end_path_msq
+    path_final_msq = ""
+    for elements in x:
+        if elements != " ":
+            path_final_msq+= elements
+    commande_py = 'move *.py "' + path_final_msq + '"'
+    commande_c = 'move *.c "' + path_final_msq + '"'
+    #on vérifie si un fichier existe déjà, sinon on le créer
+    if os.path.exists(path_final_msq):
+        print("dossier déjà existant")
+    else:
+        os.mkdir(path_final_msq)
+        print("Dossier non existant, création du dossier en cours")
+    os.system(commande_py)
+    os.system(commande_c)
+
+#tout trier
 def tout_trier():
     """fonction qui trie tout d'un coup"""
     trieur_setup()
@@ -147,31 +210,51 @@ def tout_trier():
     trieur_texte()
     trieur_musique()
     trieur_packages()
+    trieur_font()
+    trieur_videos()
+
+"""
+#creation de la bar de menu
+menu_bar = tkinter.Menu(master)
+#creation des menus interieurs
+file_menu = tkinter.Menu(menu_bar,tearoff=0)
+file_menu.add_command(label="Fichier")
+menu_bar.add_cascade(label="Fichier", menu=file_menu)
+"""
 
 """CREATION DE L'INTERFACE GRAPHIQUE"""
 
 #bouton trier exe 
-button_exe = tkinter.Button(master,text="Trier les fichiers exe/setup",font=30,command=trieur_setup,bg='#888585',fg='white')
-button_exe.pack()
+button_exe = tkinter.Button(master,text="Trier les fichiers exe/setup",font=('Calibri',11),command=trieur_setup,bg='white',fg='black')
+button_exe.pack(expand=0.25)
 
 #bouton trier images
-button_img = tkinter.Button(master,text="Trier les fichiers image : jpg/jpeg",font=30,command=trieur_image,bg='#888585',fg='white')
-button_img.pack()
+button_img = tkinter.Button(master,text="Trier les fichiers image : jpg/jpeg",font=('Calibri',11),command=trieur_image)
+button_img.pack(expand=0.25)
 
 #bouton trier textes
-button_txt = tkinter.Button(master,text="Trier les fichiers de texte",font=30,command=trieur_texte,bg='#888585',fg='white')
-button_txt.pack()
+button_txt = tkinter.Button(master,text="Trier les fichiers de texte",font=('Calibri',11),command=trieur_texte)
+button_txt.pack(expand=0.25)
 
 #bouton trier musique
-button_msq = tkinter.Button(master,text="Trier les fichiers musique",font=30,command=trieur_musique,bg='#888585',fg='white')
-button_msq.pack()
+button_msq = tkinter.Button(master,text="Trier les fichiers musique",font=('Calibri',11),command=trieur_musique)
+button_msq.pack(expand=0.25)
 
 #bouton trier packages
-button_packg = tkinter.Button(master,text="Trier les fichiers packages",font=30,command=trieur_packages,bg='#888585',fg='white')
-button_packg.pack()
+button_packg = tkinter.Button(master,text="Trier les fichiers packages",font=('Calibri',11),command=trieur_packages)
+button_packg.pack(expand=0.25)
+
+#bouton trier vidéos
+button_video = tkinter.Button(master,text="Trier les fichiers vidéos",font=('Calibri',11),command=trieur_videos)
+button_video.pack(expand=0.25)
+
+#bouton trier font
+button_font = tkinter.Button(master,text="Trier les fichiers Polices d'écriture",font=('Calibri',11),command=trieur_font)
+button_font.pack(expand=0.25)
 
 #bouton TOUT TRIER
-button_all = tkinter.Button(master,text="Tout trier",font=10,command=tout_trier, height = 4, width = 7,bg='#888585',fg='white')
+button_all = tkinter.Button(master,text="Tout trier",font=('Calibri',11),command=tout_trier, height = 4, width = 7)
 button_all.pack(expand=1)
 
+#master.config(menu=menu_bar)
 tkinter.mainloop()
--- pierre feuille ciseaux/main_pfc.py
+++ pierre feuille ciseaux/main_pfc.py
@@ -2,12 +2,14 @@ import tkinter
 import random
 import fonctions_pfc
 import time
+from tkinter import font
 
 #creation de la fenêtre
 master = tkinter.Tk()
 master.geometry("720x480")
 master.maxsize(720,480)
 master.minsize(720,480)
+master.title("Jeu du Pierre Feuille Ciseaux")
 master.config(background="#895E42")
 
 #importation des images
@@ -16,11 +18,10 @@ hauteur = 300
 image_boutonPierre = tkinter.PhotoImage(file="pierre.gif").zoom(20).subsample(40)
 image_boutonFeuille = tkinter.PhotoImage(file="papier.gif").zoom(20).subsample(40)
 image_boutonCiseaux = tkinter.PhotoImage(file="ciseaux.gif").zoom(20).subsample(40)
+image_boutonHome = tkinter.PhotoImage(file="bouton_jouer.png").zoom(40).subsample(40)
 
-
+#initialisation du choix du joueur par défault = 0
 usr_choice = 0
-time_wait = 2000
-x = "bonjour"
 
 
 #DISPLAY ET UNDISPLAY DES ELEMENTS DE L'ECRAN
@@ -28,6 +29,7 @@ x = "bonjour"
 #UNDISPLAY
 def undisplay_pack_component():
     """ retirer tout ce qu'il y a à l'écran"""
+    label_reprise_jeu.pack_forget()
     label_pierre.pack_forget()
     boutonPierre.pack_forget()
     label_feuille.pack_forget()
@@ -38,11 +40,17 @@ def undisplay_pack_component():
     LabelResultats.pack_forget()
     LabelScorePc.pack_forget()
     LabelScoreJoueur.pack_forget()
+    whatPlayerDoes_Button.pack_forget()
+    whatPcDoes_Button.pack_forget()
+    label_usr_plays.pack_forget()
+    label_pc_plays.pack_forget()
+
 #DISPLAY
 def display_pack_component():
     """on affiche tous les composants à l'écran"""
     boutonEnd.pack_forget()
     bouttonHome.pack_forget()
+    label_reprise_jeu.pack()
     label_pierre.pack()
     boutonPierre.pack()
     label_feuille.pack()
@@ -50,41 +58,76 @@ def display_pack_component():
     label_ciseaux.pack()
     boutonCiseaux.pack()
     boutonJouer.pack()
+
+    label_usr_plays.pack(side='right')
+    whatPlayerDoes_Button.pack(side="right")
+
+    label_pc_plays.pack(side="left")
+    whatPcDoes_Button.pack(side="left")
+
     LabelResultats.pack(expand="YES")
     LabelScorePc.pack(side='left')
     LabelScoreJoueur.pack(side='right')
 
+
 def clickPierre():
+    """action cliquer sur bouton pierre"""
     global usr_choice
     usr_choice = 1
     print("votre choix : ",usr_choice)
     disable()
 
 def clickFeuille():
+    """action cliquer sur bouton feuille/papier"""
     global usr_choice
     usr_choice = 2
     print("votre choix : ",usr_choice)
     disable()
 
 def clickCiseaux():
+    """action cliquer sur bouton ciseaux"""
     global usr_choice
     usr_choice = 3
     print("votre choix : ",usr_choice)
     disable()
 
+
 def jeu():
+    """fonction principale qui lance le jeu/contient le code principale"""
+
+    #le pc choisit un nombre
     pc_choice = random.randint(1,3)
+
+    #on change l'image en fonction de ce que montre le pc
+    if pc_choice == 1:
+        whatPcDoes_Button.config(image = image_boutonPierre)
+    elif pc_choice == 2:
+        whatPcDoes_Button.config(image = image_boutonFeuille)
+    else:
+        whatPcDoes_Button.config(image = image_boutonCiseaux)
+
+    #on change l'image en fonction de ce que montre le joueur
+    if usr_choice == 1:
+        whatPlayerDoes_Button.config(image = image_boutonPierre)
+    elif usr_choice == 2:
+        whatPlayerDoes_Button.config(image = image_boutonFeuille)
+    else:
+        whatPlayerDoes_Button.config(image = image_boutonCiseaux)
+
+    #actions sur la console
     print("le joueur choisit : ", usr_choice)
     print("le pc choisit : ", pc_choice)
+    #application des règles du jeu
     fonctions_pfc.regles_jeu(usr_choice,pc_choice)
+    #on réactive les boutons pierre feuille et ciseaux
     enable()
 
     #on affiche si le joueur perd ou gagne
     LabelResultats.config(text=fonctions_pfc.result)
 
     #on affiche le score du pc
-    LabelScorePc.config(text=fonctions_pfc.pc_score)
-    LabelScoreJoueur.config(text=fonctions_pfc.usr_score)
+    LabelScorePc.config(text=fonctions_pfc.pc_score,font=("Berlin Sans FB Demi",15))
+    LabelScoreJoueur.config(text=fonctions_pfc.usr_score,font=("Berlin Sans FB Demi",15))
 
     #FIXER LA LIMITE DU SCORE POUR GAGNER -->le premier arrivé à 10 à gagner
     if fonctions_pfc.y == 5:
@@ -97,30 +140,32 @@ def jeu():
         boutonEnd.pack(expand='YES')
 
 def goToZero():
+    """fonction de reboot du jeu"""
+    #on remet tout à zero
     usr_choice = 0
     fonctions_pfc.x = 0
     fonctions_pfc.y = 0
-    result = ""
+    fonctions_pfc.result = ""
     fonctions_pfc.pc_score = " SCORE DU PC : "
     fonctions_pfc.usr_score = " SCORE DU PC : "
-
+    #on ré-affiche tous les composants
     display_pack_component()
 
-
-
+#fonctions bloquer/débloquer les boutons
 def disable():
+    """bloquer les boutons pierre feuille et ciseaux"""
     boutonPierre["state"] = "disabled"
     boutonFeuille["state"] = "disabled"
     boutonCiseaux["state"] = "disabled"
 
 def enable():
+    """bloquer les boutons pierre feuille et ciseaux"""
     boutonPierre["state"] = "normal"
     boutonFeuille["state"] = "normal"
     boutonCiseaux["state"] = "normal"
 
-
-
 #création et insertion des Label/boutons
+
 label_pierre = tkinter.Label(master,text="Pierre",bg="#895E42",fg="white")
 
 boutonPierre = tkinter.Button(master,text="Pierre",image=image_boutonPierre,command=clickPierre)
@@ -134,23 +179,33 @@ label_ciseaux = tkinter.Label(master,text="Ciseaux",bg="#895E42",fg="white")
 boutonCiseaux = tkinter.Button(master,image=image_boutonCiseaux,command=clickCiseaux)
 
 #BOUTONS NON JOUABLES, cad afficher resultat, et valider le coup
-boutonJouer = tkinter.Button(master,text="MONTRER",fg="white",bg="#895E42",command=jeu)
+boutonJouer = tkinter.Button(master,text="MONTRER",fg="white",bg="#895E42",font=("Berlin Sans FB Demi",10),command=jeu)
 
-LabelResultats = tkinter.Label(master,fg="white",bg="#895E42",font=40)
+LabelResultats = tkinter.Label(master,fg="white",bg="#895E42",font=("Berlin Sans FB Demi",15))
 
 #afficher les scores à l'écran
-LabelScorePc = tkinter.Label(master,fg="white",bg="#895E42",font=40)
+LabelScorePc = tkinter.Label(master,fg="white",bg="#895E42",font=("Berlin Sans FB Demi",15))
 
-LabelScoreJoueur = tkinter.Label(master,fg="white",bg="#895E42",font=40)
+LabelScoreJoueur = tkinter.Label(master,fg="white",bg="#895E42",font=("Berlin Sans FB Demi",15))
 
 
 #PAGE ACCUEIL AVEC BOUTON JOUER
-bouttonHome = tkinter.Button(master,text="LANCER LE JEU ! ",bg="#895E42",fg="white",font=100,command=display_pack_component)
-bouttonHome.pack()
+bouttonHome = tkinter.Button(master,text="LANCER LE JEU ! ",image=image_boutonHome,bg="#895E42",fg="white",font=font.Font(family='Arial', size=30),command=display_pack_component)
+bouttonHome.pack(expand="yes")
 
 #PAGE FINAL AVEC AFFICHAGE NOM DU VAINQUEUR, AINSI QUE LE BOUTON POUR PRENDRE UNE REVANCHE
-boutonEnd = tkinter.Button(master,text="PRENEZ VOTRE REVANCHE ICI !",bg="#895E42",fg="white",font=100,command= goToZero)
+boutonEnd = tkinter.Button(master,text="PRENEZ VOTRE REVANCHE ICI !",bg="#895E42",fg="white",font=("Berlin Sans FB Demi",30),command= goToZero)
+label_reprise_jeu = tkinter.Label(master,text="JEU DU PIERRE FEUILLE CISEAUX ",bg="#895E42",fg="white",font=("Berlin Sans FB Demi",15))
 
+
+#que joue le pc ?
+whatPcDoes_Button = tkinter.Label(master)
+label_pc_plays = tkinter.Label(master,text="LE PC JOUE : ",bg="#895E42",fg="white",font=("Berlin Sans FB Demi",15))
+
+
+#que joue le joueur ?
+whatPlayerDoes_Button = tkinter.Label(master)
+label_usr_plays = tkinter.Label(master,text="VOUS JOUEZ : ",bg="#895E42",fg="white",font=("Berlin Sans FB Demi",15))
 
 #affichage final de la fenêtre
 tkinter.mainloop()
